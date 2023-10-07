ba3875a1be984fa4a925eee03093d1decbc5eda2
magdalenahuget <magdalena.huget@gmail.com>
2023-10-07 16:15:12 +0200

    Initial commit

---
+++ src/main/java/com/company/mykongspringv1/MykongSpringV1Application.java
@@ -0,0 +1,13 @@
+package com.company.mykongspringv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykongSpringV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MykongSpringV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/mykongspringv1/MykongSpringV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.mykongspringv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MykongSpringV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
